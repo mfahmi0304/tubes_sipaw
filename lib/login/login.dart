f9ba971116224186dc9eb9fa77b27acd0a7b5b3e
mramdan99 <muhammad.ramdan1412@gmail.com>
2020-12-20 15:46:33 +0700

    update login

---
+++ lib/login/login.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:tubes_sipaw/register.dart';
+import 'package:tubes_sipaw/home.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController usernameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFFEEEEEE),
+        brightness: Brightness.light,
+        title: Text('LOGIN',
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            )),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.only(
+          // top: 0,
+          bottom: 10,
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 10,
+                left: 20,
+                right: 20,
+              ),
+              child: TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  labelStyle: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 10,
+                left: 20,
+                right: 20,
+              ),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                autofocus: false,
+                obscureText: true,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: FlatButton(
+                child: Text('Masuk'),
+                color: Colors.blueAccent,
+                textColor: Colors.white,
+                onPressed: () {
+                  if (usernameController.text == '' ||
+                      passwordController.text == '') {
+                    showToast('Masukkan username atau password');
+                  } else {
+                    if (usernameController.text == 'Ramdan' &&
+                        passwordController.text == '123456') {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Dashboard(
+                            username: usernameController.text,
+                            password: passwordController.text,
+                          ),
+                        ),
+                      );
+                    } else {
+                      showToast('Username Password Salah');
+                    }
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showToast(text) {
+    Fluttertoast.showToast(
+        msg: text,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black.withOpacity(0.5),
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+}
