d8937119c0f35e7aed03ca8ccb2d2e9fbcd2a2c7
unknown <m.fahminuraziz93@gmail.com>
2020-12-20 08:52:05 +0700

    [UPDATE]

---
+++ lib/awal.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AwalPage extends StatefulWidget {
+  @override
+  _AwalPageState createState() => _AwalPageState();
+}
+
+class _AwalPageState extends State<AwalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Halaman Awal'),
+      ),
+      
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:tubes_sipaw/awal.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState(){
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async{
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_){
+          return AwalPage();
+        })
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/Logo.png",
+              width: 200,
+              height: 200,
+              alignment: Alignment.center,
+            ),
+            Text(
+              'Sipaw',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
