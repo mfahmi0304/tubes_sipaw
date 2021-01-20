b6b6a3669dd0a8fba067b31b785a30281531856c
unknown <m.fahminuraziz93@gmail.com>
2021-01-21 02:09:06 +0700

    [UPDATE]

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart'; 
+import 'package:firebase_core/firebase_core.dart'; 
 import 'package:flutter/material.dart';
+import 'package:tubes_sipaw/libservices/authentication_service.dart';
 import 'package:tubes_sipaw/splashscreen.dart';
+import 'package:provider/provider.dart'; 
 
-void main() {
-  runApp(MyApp());
-}
+Future<void> main() async { 
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(); 
+  runApp(MyApp()); 
+} 
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-    debugShowCheckedModeBanner: false,
-      home: SplashScreenPage(),
-    );
-  }
-}
+class MyApp extends StatelessWidget { 
+  // This widget is the root of your application. 
+  @override 
+  Widget build(BuildContext context) { 
+    return MultiProvider( 
+      providers: [ 
+        Provider<AuthenticationService>( 
+          create: (_) => AuthenticationService(FirebaseAuth.instance), 
+        ), 
+        StreamProvider( 
+            create: (context) => 
+                context.read<AuthenticationService>().authStateChanges), 
+      ], 
+      child: MaterialApp( 
+        home: SplashScreenPage(), 
+      ), 
+    ); 
+  } 
+} 
